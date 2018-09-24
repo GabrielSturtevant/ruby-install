51924b59ff667b3fa51c801588c8e66af9eb4767
Gabriel Sturtevant <Gabriel.Sturtevant@BlackLine.com>
2018-09-24 10:23:19 -0700

    Commit

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 UNICORN_INSTALLED=$(cat Gemfile | grep "gem 'unicorn'" | wc -l)
 if [ "$UNICORN_INSTALLED" -lt 1 ]; then
-    echo "Less than 1"
-else
-    echo "Unicorn Installed: $UNICORN_INSTALLED"
+    echo "Adding unicorn to Gemfile"
+    echo "gem 'unicorn'" >> Gemfile
 fi
