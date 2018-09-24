f74e88cbc183345f417865a2db64c459dc4ce631
Gabriel Sturtevant <Gabriel.Sturtevant@BlackLine.com>
2018-09-24 11:40:11 -0700

    Commit

--- install.sh
+++ install.sh
@@ -5,9 +5,15 @@ if [ "$UNICORN_INSTALLED" -lt 1 ]; then
     echo "gem 'unicorn'" >> Gemfile
 fi
 bundle
+
+echo Creating config and init files
 # TODO(Gabriel): Update these links to the internal links
 wget -qO- https://raw.githubusercontent.com/GabrielSturtevant/ruby-install/master/unicorn.rb >> config/unicorn.rb
-wget -qO- https://raw.githubusercontent.com/GabrielSturtevant/ruby-install/master/unicorn-init.sh | sudo tee /etc/init.d/unicorn_oracle_connector
+wget -qO- https://raw.githubusercontent.com/GabrielSturtevant/ruby-install/master/unicorn-init.sh | sudo tee /etc/init.d/unicorn_oracle_connector > /dev/null
 
+echo Updating init file permissions
 sudo chmod 755 /etc/init.d/unicorn_oracle_connector
 sudo update-rc.d unicorn_oracle_connector defaults
+
+
+echo Done. Goodbye
