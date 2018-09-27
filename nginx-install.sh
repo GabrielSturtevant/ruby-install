88c064005ff150999965a4ad702cd2e9114fd507
Gabriel Sturtevant <Gabriel.Sturtevant@BlackLine.com>
2018-09-27 08:28:02 -0700

    Commit

---
+++ nginx-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export APP_NAME="example"
+sudo apt-get install nginx -y
+wget -qO- https://raw.githubusercontent.com/GabrielSturtevant/ruby-install/master/nginx-config > nginx-config
+sed -ir "s/deploy/$USER/g" nginx-config
+sed -ir "s/appname/$APP_NAME/g" nginx-config
+sudo mv nginx-config /etc/nginx/sites-available/default
+
